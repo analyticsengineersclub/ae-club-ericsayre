8ce49d43fd662fdf2e79acaf32af24a2cdef5a6f
Ubuntu <ubuntu@ip-172-31-16-114.us-east-2.compute.internal>
2021-10-14 04:36:12 +0000

    UPD: just formatting; generating merge conflict

--- customers.sql
+++ customers.sql
@@ -1,9 +1,9 @@
 select
-cust.id customer_id
-,cust.name
-,cust.email
-,min(ord.created_at) first_order_at
-,count(ord.id) number_of_orders
+  cust.id customer_id
+, cust.name
+, cust.email
+, min(ord.created_at) first_order_at
+, count(ord.id) number_of_orders
 from `analytics-engineers-club.coffee_shop.customers` cust
 left join `analytics-engineers-club.coffee_shop.orders` ord
 on cust.id = ord.customer_id
